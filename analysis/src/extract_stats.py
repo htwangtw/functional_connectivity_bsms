127a422862862b9dd1378604c0d76c2e11e140a1
Hao-Ting Wang <htwangtw@gmail.com>
2021-05-28 17:33:52 +0100

    FIX use first level contrast for all stats

--- analysis/src/extract_stats.py
+++ analysis/src/extract_stats.py
@@ -20,30 +20,31 @@ thresh_z_paths = [(project_path /
     ]
 
 subject_stats_path = [
-    [str(p) for p in project_path.glob("results/subject_level/sub-*/ses-placebo/icbm_insula_effect_size.nii.gz")],
-    [str(p) for p in project_path.glob("results/subject_level/sub-*/icbm_insula_typhoid_wrt_placebo_effect_size.nii.gz")],
+    [str(p) for p in project_path.glob("results/subject_level/sub-*/ses-typhoid/icbm_insula_effect_size.nii.gz")],
+    [str(p) for p in project_path.glob("results/subject_level/sub-*/ses-placebo/icbm_insula_effect_size.nii.gz")]
     ]
 
 beta_weight = pd.read_csv(project_path / "analysis/group_design.csv", index_col=0)
 
-for map_path, subject_stats in zip(thresh_z_paths, subject_stats_path):
+for map_path in thresh_z_paths:
     thresh_z = nb.load(str(map_path))
+    analysis_name = str(map_path.parent).split("/")[-1] + "_" + map_path.name.split("_thresh")[0]
     bin_cluster = math_img("(img > 0).astype(int)", img=thresh_z)
     label_regions = connected_label_regions(bin_cluster)
-    masker = NiftiLabelsMasker(label_regions)
-    cluster_data = masker.fit_transform(subject_stats)
-
-    cisc_id = [
-        int(sub.split("sub-")[-1].split("/")[0])
-        for sub in subject_stats
-    ]
-    analysis_name = str(map_path.parent).split("/")[-1] + "_" + map_path.name.split("_thresh")[0]
-
     label_regions.to_filename(str(project_path / f"results/group_level/{analysis_name}_cluster_label.nii.gz"))
 
-    data = pd.DataFrame(cluster_data, index=cisc_id)
-    col_name = [f"{analysis_name}_cluster_{col + 1:02d}"for col in data.columns]
-    data.columns = col_name
-    beta_weight = pd.concat([beta_weight, data], axis=1)
+    masker = NiftiLabelsMasker(label_regions)
+
+    for ses in subject_stats_path:
+        session_name = ses[0].split("ses-")[-1].split("/")[0]
+        cluster_data = masker.fit_transform(ses)
+        cisc_id = [
+            int(sub.split("sub-")[-1].split("/")[0])
+            for sub in ses
+        ]
+        data = pd.DataFrame(cluster_data, index=cisc_id)
+        col_name = [f"ses-{session_name}_{analysis_name}_cluster_{col + 1:02d}"for col in data.columns]
+        data.columns = col_name
+        beta_weight = pd.concat([beta_weight, data], axis=1)
 
 beta_weight.to_csv(project_path / "results/group_level/insula_connectivity_beta_weight.csv")
--- results/README.md
+++ results/README.md
@@ -1,2 +1,36 @@
 # What's in the results directory
 
+General organisation:
+
+```
+results/
+├── group_level/
+│   ├── <analysis_name>/
+│   │   ├── <contrast_name>_thresh_zstat.nii.gz
+│   │   ├── <contrast_name>_zstat.nii.gz
+│   │   ├── inputs.csv
+│   │   └── <analysis_type>_design-matrix.csv
+│   ├── <analysis_name>_<contrast_name>_cluster_label.nii.gz
+│   └── insula_connectivity_beta_weight.csv
+├── subject_level/
+│   ├── sub-<cisc_id>/
+│   │   ├── ses-<session>/
+│   │   │   ├── <seed_name>_effect_size.nii.gz
+│   │   │   ├── <seed_name>_effect_variance.nii.gz
+│   │   │   ├── <seed_name>_p_value.nii.gz
+│   │   │   ├── <seed_name>_stat.nii.gz
+│   │   │   └── <seed_name>_z_score.nii.gz
+│   │   └── <seed_name>_typhoid_wrt_placebo_effect_size.nii.gz
+│   └── sub-<cisc_id>_ses-<session>_seed-<seed_name>.html
+├── <analysis_name>.html
+└── README.md
+
+```
+
+Group level results are `group_level/<analysis_name>.html` for quick access.
+Significant custer label mask: `group_level/<analysis_name>_<contrast_name>_cluster_label.nii.gz`.
+Find the relevant threshould stats map: `group_level/<analysis_name>/<contrast_name>_thresh_zstat.nii.gz`
+Use**threshould stats map** for visualisation.
+
+For significant clusters of all analysis, the beta-weight (coefficient scores from the relevant first level contrast `<seed_name>_effect_size.nii.gz`) are stored in `group_level/insula_connectivity_beta_weight.csv`.
+These results can be used for further analysis.
