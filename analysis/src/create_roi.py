bce0aeb008b491450e6174cb2f9f5ab9d8f0396d
Hao-Ting Wang <htwangtw@gmail.com>
2021-07-01 10:31:22 +0100

    Split insula to left and right; add idx 161

--- analysis/src/create_roi.py
+++ analysis/src/create_roi.py
@@ -1,4 +1,4 @@
-"""Create insula ROI
+"""Create ROIs from Glasser atlas index
 Authors: Marisa Amato and Hao-Ting Wang
 Date: May 13, 2021
 """
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import numpy as np
 import nibabel as nb
+from nilearn.regions import connected_label_regions
 # from nilearn import plotting
 
 # define paths
@@ -15,16 +16,34 @@ roi_path = atlas_dir / "icbm_insula.nii.gz"
 
 # labels of ROI in the atlas
 glasser_insula = [103, 106, 108, 109, 110, 111, 112, 114, 115, 167, 168]
+glasser_cingulate = [161]
 
-if __name__ == "__main__":
+
+def creat_roi_nifti(atlas_path, roi_path, glasser_index):
+    """ROI from icmb glasser atlas index."""
     atlas = nb.load(str(atlas_path))
     atlas_data = atlas.get_fdata().astype(int)
 
-    insula_mask = np.zeros(atlas.shape)
-    for id in glasser_insula:
+    roi_mask = np.zeros(atlas.shape)
+    for id in glasser_index:
         current_region = np.where(atlas_data == id, id, 0)
-        insula_mask += current_region
+        roi_mask += current_region
 
-    insula_mask = (insula_mask > 0).astype(int)
-    insula_nifti = nb.Nifti1Image(insula_mask, affine=atlas.affine, header=atlas.header)
-    insula_nifti.to_filename(str(roi_path))
+    roi_mask = (roi_mask > 0).astype(int)
+    roi_nifti = nb.Nifti1Image(roi_mask, affine=atlas.affine, header=atlas.header)
+    roi_nifti.to_filename(str(roi_path))
+
+
+if __name__ == "__main__":
+    creat_roi_nifti(atlas_path,
+                    atlas_dir / "icbm_insula-both.nii.gz",
+                    glasser_insula)
+    icbm_insula = connected_label_regions(
+        str(atlas_dir / "icbm_insula-both.nii.gz"),
+        min_size=500)
+    icbm_insula.to_filename(atlas_dir / "icbm_insula-both.nii.gz")
+    creat_roi_nifti(atlas_dir / "icbm_insula.nii.gz", atlas_dir / "icbm_insula-R.nii.gz", [1])
+    creat_roi_nifti(atlas_dir / "icbm_insula.nii.gz", atlas_dir / "icbm_insula-L.nii.gz", [2])
+    creat_roi_nifti(atlas_path,
+                    atlas_dir / "icbm_cingulate_idx-161.nii.gz",
+                    glasser_cingulate)
